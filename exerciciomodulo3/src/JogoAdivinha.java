d56bc6e1d1a4bb80868ae3ddf81e5624e99c3f30
isabela.vsilva@bandtec.com.br <isabela.vsilva@bandtec.com.br>
2024-08-04 16:06:28 -0300

    Ajustes na logica do desafio

--- exerciciomodulo3/src/JogoAdivinha.java
+++ exerciciomodulo3/src/JogoAdivinha.java
@@ -4,28 +4,27 @@ import java.util.Scanner;
 public class JogoAdivinha {
     public static void main(String[] args) {
         Scanner leitura = new Scanner(System.in);
-
-        int numeroDigitado;
+        int numeroDigitado = 0;
         int numeroAleatorio = new Random().nextInt(100);
         int tentativas = 5;
 
-        while (tentativas != 0) {
+        while (tentativas > 0) {
             System.out.println("Digite o número: ");
             numeroDigitado = leitura.nextInt();
             tentativas--;
 
             if (numeroDigitado == numeroAleatorio) {
-                System.out.println("Número aleatorio: " + numeroAleatorio + " é igual ao número " + "Número digitado: " + numeroDigitado + " Número de tentativas: " + tentativas);
+                System.out.println("Parabéns você acertou o número aleatorio! Com apenas " + tentativas + "tentativas");
                 break;
-
+            } else if (numeroDigitado > numeroAleatorio) {
+                System.out.println("Número Aleatorio é menor. Você tem: " + tentativas + " tentativas");
             } else {
-                if (numeroDigitado > numeroAleatorio) {
-                    System.out.println("Número Aleatorio é menor. Você tem: " + tentativas + " tentativas");
-                } else {
-                    System.out.println("Númeor Aleatorio é maior. Você tem: " + tentativas + " tentativas");
-                }
+                System.out.println("Número Aleatorio é maior. Você tem: " + tentativas + " tentativas");
+            }
+            if (tentativas == 0) {
+                System.out.println("Infelizmente você não conseguiu acerta o número e acabou suas tentativas. O número era: " + numeroAleatorio);
             }
         }
-        System.out.println("Infelizmente suas chances acabaram, o número aleatorio era: " + numeroAleatorio);
+
     }
 }
