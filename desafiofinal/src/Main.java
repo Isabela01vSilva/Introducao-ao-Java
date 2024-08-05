defea04e39d452c82948b8d191792c4a6a4557a6
isabela.vsilva@bandtec.com.br <isabela.vsilva@bandtec.com.br>
2024-08-05 10:07:57 -0300

    Organzinando projeto final

--- desafiofinal/src/Main.java
+++ desafiofinal/src/Main.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class Desafio {
+public class Main {
     public static void main(String[] args) {
         String nome = "Isabela";
         String tipoDeConta = "Corrente";
